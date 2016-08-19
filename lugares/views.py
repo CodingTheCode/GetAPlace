e3565028fafc4b5a77f5e85805cfa970c13658ca
Laury Bueno <laury@hacklab.com.br>
2016-08-19 18:18:04 -0300

    API base para listar os 30 locais criados mais recentemente

--- lugares/views.py
+++ lugares/views.py
@@ -3,9 +3,16 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from lugares.models import Local
+from lugares.serializers import LocalSerializer
+
+cabecalhos = {'Access-Control-Allow-Origin': '*', }
 
 
 @csrf_exempt
 @api_view(['GET'])
 def lugares_list(request):
-    pass
+
+    locais = Local.objects.all()[:30]
+    serializer = LocalSerializer(locais, many=True)
+    return Response(serializer.data, headers=cabecalhos)

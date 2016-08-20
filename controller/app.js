766bccf87848f10260db9764d1897393af5e06f6
Laury Bueno <laury@hacklab.com.br>
2016-08-20 11:17:25 -0300

    SSL nas invocações XHR

--- controller/app.js
+++ controller/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp',['ngResource']).factory('Local', function ($resource) {
-    return $resource('http://getaplace.herokuapp.com/api/lugares/');
+    return $resource('https://getaplace.herokuapp.com/api/lugares/');
 }).factory('LocalEspecifico', function ($resource) {
-  return $resource('http://getaplace.herokuapp.com/api/lugares/:id');
+  return $resource('https://getaplace.herokuapp.com/api/lugares/:id');
 }).factory('ReviewsLocal', function ($resource){
   return $resource('https://getaplace.herokuapp.com/api/lugares/reviews/:id');
 }).factory('BuscaTags', function ($resource){

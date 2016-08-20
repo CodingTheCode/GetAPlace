ac092a6e3f6b45709e4266597c94259f849a8b2a
Lucas Delboni <lucas_cd460@hotmail.com>
2016-08-20 03:52:50 -0300

    iniciada a tela de login

--- controller/app.js
+++ controller/app.js
@@ -6,6 +6,8 @@ var app = angular.module('myApp',['ngResource']).factory('Local', function ($res
   return $resource('https://getaplace.herokuapp.com/api/lugares/reviews/:id');
 }).factory('BuscaTags', function ($resource){
   return $resource('https://getaplace.herokuapp.com/api/lugares/busca/nome/:nome/endereco/:endereco/bairro/:bairro/cidade/:cidade/estado/:estado/pais/:pais/tags/:tags/');
-  
 })
 ;
+
+
+var login = angular.module('myLogin'){};

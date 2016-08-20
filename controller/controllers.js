3eb3862c088ac21e034cf435411a6cb6b61eb5ce
leopiccionia <leopiccionia@gmail.com>
2016-08-20 07:54:26 -0300

    Juntando controllers

--- controller/controllers.js
+++ controller/controllers.js
@@ -1,22 +1,18 @@
-app.controller('ResourceController', function($scope, Local) {
+app.controller('ResourceController', function($scope, Local, LocalEspecifico) {
   var locais = Local.query(function() {
-    //console.log(locais);
+    console.log(locais);
   });
   $scope.espacos = locais;
-});
-
-app.controller('LocalUnicoController',function($scope, LocalEspecifico) {
-  $scope.id=1;
-  var entry = LocalEspecifico.get({ id: $scope.id }, function() {
-    //console.log(entry);
-  });
-  $scope.espaco = entry;
+  
+  $scope.atualizaId = function(id){
+    $scope.espaco = $scope.espacos[id];
+  };
 });
 
 app.controller('ReviewsLocalController', function($scope, ReviewsLocal){
   $scope.id=1;
   var reviews = ReviewsLocal.query({ id: $scope.id }, function() {
-    console.log(reviews);
+    //console.log(reviews);
   });
   $scope.reviews = reviews;
 });

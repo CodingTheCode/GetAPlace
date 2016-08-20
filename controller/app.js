50ba94a9a631beec8c0ba3800b820da8e19dc528
Lucas Delboni <lucas_cd460@hotmail.com>
2016-08-20 07:36:39 -0300

    agora a busca realmente preenche os dados

--- controller/app.js
+++ controller/app.js
@@ -7,9 +7,6 @@ var app = angular.module('myApp',['ngResource']).factory('Local', function ($res
 }).factory('BuscaTags', function ($resource){
   return $resource('https://getaplace.herokuapp.com/api/lugares/busca/nome/:nome/endereco/:endereco/bairro/:bairro/cidade/:cidade/estado/:estado/pais/:pais/tags/:tags/');
 }).factory('AdicionaReview', function ($resource){
-  return $resource('https://getaplace.herokpais/tags/:tags/');
+  return $resource('https://getaplace.herokpais/api/locacao/reviews');
 })
 ;
-
-
-var login = angular.module('myLogin', []);
--- controller/controllers.js
+++ controller/controllers.js
@@ -1,6 +1,6 @@
 app.controller('ResourceController', function($scope, Local) {
   var locais = Local.query(function() {
-    //console.log(locais);
+    console.log(locais);
   });
   $scope.espacos = locais;
 });
@@ -16,37 +16,33 @@ app.controller('LocalUnicoController',function($scope, LocalEspecifico) {
 app.controller('ReviewsLocalController', function($scope, ReviewsLocal){
   $scope.id=1;
   var reviews = ReviewsLocal.query({ id: $scope.id }, function() {
-    console.log(reviews);
+    //console.log(reviews);
   });
   $scope.reviews = reviews;
 });
 
 app.controller('BuscaTagsController', function($scope, BuscaTags){
-  $scope.nome='mesa de bilhar';
-  $scope.endereco='_';
-  $scope.bairro='_';
-  $scope.cidade='_';
-  $scope.estado='_';
-  $scope.pais='_';
-  $scope.tag='_';
+  //$scope.nome='mesa de bilhar';
+  //$scope.endereco='_';
+  // $scope.bairro='_';
+  // $scope.cidade='_';
+  // $scope.estado='_';
+  // $scope.pais='_';
+    $scope.tag='_';
 
 
   $scope.pesquisarTags = function() {
     var reviews = BuscaTags.query({ id: $scope.id ,nome: $scope.nome, endereco: $scope.endereco, bairro: $scope.bairro, cidade: $scope.bairro, estado: $scope.estado, pais: $scope.pais, tags: $scope.tag }, function() {
       //nome, endereco, bairro, cidade, estado, pais, tag(separado por virgulas)
-      console.log(reviews);
+      //console.log(reviews);
     });
       $scope.espaco = reviews;
   };
 });
 
 app.controller('AdicionaReviewController', function($scope, AdicionaReview){
-  var reviews = ReviewsLocal.query({ id: $scope.id }, function() {
-    //console.log(reviews);
+  var reviews = ReviewsLocal.query({local: $scope.local, autor: $scope.idAutor,  titulo: $scope.titulo, texto: $scope.texto, avaliacao: $scope.avaliacao}, function() {
+    //console.log(reviews
   });
   $scope.reviews = reviews;
-});
-
-login.controller("logar", function($scope){
-  //login
 });

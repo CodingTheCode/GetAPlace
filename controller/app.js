9f006052f1821166d5bed8791347beb921678a27
Lucas Delboni <lucas_cd460@hotmail.com>
2016-08-20 01:19:10 -0300

    consumindo a api para ver reviews de um local

--- controller/app.js
+++ controller/app.js
@@ -2,4 +2,7 @@ var app = angular.module('myApp',['ngResource']).factory('Local', function ($res
     return $resource('http://getaplace.herokuapp.com/api/lugares/');
 }).factory('LocalEspecifico', function ($resource) {
   return $resource('http://getaplace.herokuapp.com/api/lugares/:id');
-});
+}).factory('ReviewsLocal', function ($resource){
+  return $resource('https://getaplace.herokuapp.com/api/lugares/reviews/:id');
+})
+;
--- controller/controllers.js
+++ controller/controllers.js
@@ -1,6 +1,6 @@
 app.controller('ResourceController', function($scope, Local) {
   var locais = Local.query(function() {
-    console.log(locais);
+    //console.log(locais);
   });
   $scope.espacos = locais;
 });
@@ -8,8 +8,15 @@ app.controller('ResourceController', function($scope, Local) {
 app.controller('LocalUnicoController',function($scope, LocalEspecifico) {
   $scope.id=1;
   var entry = LocalEspecifico.get({ id: $scope.id }, function() {
-    console.log(entry);
+    //console.log(entry);
   });
-
   $scope.espaco = entry;
+});
+
+app.controller('ReviewsLocalController', function($scope, ReviewsLocal){
+  $scope.id=1;
+  var reviews = ReviewsLocal.query({ id: $scope.id }, function() {
+    console.log(reviews);
+  });
+  $scope.espaco = reviews;
 });

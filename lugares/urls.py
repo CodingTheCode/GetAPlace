f298a7fd7f6c609659226b413df245b10b5d3bc5
Laury Bueno <laury@hacklab.com.br>
2016-08-19 22:28:10 -0300

    API retorna reviews de um lugar por id

--- lugares/urls.py
+++ lugares/urls.py
@@ -5,5 +5,6 @@ from lugares import views
 urlpatterns = [
     url(r'^$', views.lugares_list),
     url(r'^(?P<id>[0-9]+)$', views.lugar),
+    url(r'^reviews/(?P<id>[0-9]+)$', views.reviews_lugar),
     url(r'^busca/(?P<nome_busca>[\w ]+)/$', views.lugares_busca)
 ]
--- lugares/views.py
+++ lugares/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from lugares.models import Local
-from lugares.serializers import LocalSerializer
+from lugares.serializers import LocalSerializer, ReviewSerializer
 
 
 @csrf_exempt
@@ -26,10 +26,19 @@ def lugar(request, id):
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+# Retorna todos os reviews de um local baseado em seu id
+@csrf_exempt
+@api_view(['GET'])
+def reviews_lugar(request, id):
+    reviews = Local.objects.get(id=id).reviews
+    serializer = ReviewSerializer(reviews, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 @csrf_exempt
 @api_view(['GET'])
 def lugares_busca(request, nome_busca):
 
-    locais =Local.objects.filter(nome=nome_busca)
+    locais = Local.objects.filter(nome=nome_busca)
     serializer = LocalSerializer(locais, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)

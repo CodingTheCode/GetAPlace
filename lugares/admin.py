ea35b148c2daa6f1fdfdb6094fadfaec456985f1
Jonas <jonasoliveirabarbosa@hotmail.com>
2016-08-20 05:38:09 -0300

    Adicionado modelagem da entidade aluguel

--- lugares/admin.py
+++ lugares/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Local, Atributo, Review, Imagem
+from .models import Local, Atributo, Review, Imagem, Locacao
 
 admin.site.register(Local)
 admin.site.register(Atributo)
 admin.site.register(Review)
 admin.site.register(Imagem)
+admin.site.register(Locacao)
--- lugares/models.py
+++ lugares/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from django.utils import timezone
 
 
 # Locais disponíveis para locação
@@ -42,3 +43,12 @@ class Review(models.Model):
     texto = models.TextField()
     avaliacao = models.CharField(choices=GOSTOS_CHOICES, default='regular', max_length=64)
     autor = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='reviews')
+
+# Caracteristicas de uma locação
+class Locacao(models.Model):
+    localLocacao = models.ForeignKey(Local, related_name='locacao')
+    locador =  models.ForeignKey(settings.AUTH_USER_MODEL, related_name='imovel_locado')
+    locatario =  models.ForeignKey(settings.AUTH_USER_MODEL, related_name='imovel_alugado')
+    horaInicio = models.DateField('dataInicio', default=timezone.now())
+    horaFim = models.DateField('dataFim', default=timezone.now())
+    valor = models.DecimalField(max_digits=8, decimal_places=2)

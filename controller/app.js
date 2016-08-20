a04efb84777db520be24c80e1ae2d0b9276e7e74
Lucas Delboni <lucas_cd460@hotmail.com>
2016-08-20 02:11:39 -0300

    feito consumação da api de busca com tags

--- controller/app.js
+++ controller/app.js
@@ -4,5 +4,8 @@ var app = angular.module('myApp',['ngResource']).factory('Local', function ($res
   return $resource('http://getaplace.herokuapp.com/api/lugares/:id');
 }).factory('ReviewsLocal', function ($resource){
   return $resource('https://getaplace.herokuapp.com/api/lugares/reviews/:id');
+}).factory('BuscaTags', function ($resource){
+  return $resource('https://getaplace.herokuapp.com/api/lugares/busca/nome/:nome/endereco/:endereco/bairro/:bairro/cidade/:cidade/estado/:estado/pais/:pais/tags/:tags/');
+  
 })
 ;
